7f8ff2cb8721c40f97fa89f6b0124d2aceb4e1ac
michael <michael@zarttech.com>
2022-08-19 01:58:57 +0100

    first commit
    first commit

---
+++ rewardteacher/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import ErrorBoundary from './Component/Common/ErrorBoundary';
+import React, { Suspense } from 'react';
+
+function App() {
+  return (
+    <ErrorBoundary>
+       <Suspense fallback="loading">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+       </Suspense>  
+    </ErrorBoundary>  
+  );
+}
+
+export default App;

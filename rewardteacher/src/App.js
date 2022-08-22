256f4605e9402eef4be2f0e730dd84269b531f94
Ogbonna Ikenna Sydney <cindionline@yahoo.com>
2022-08-22 20:27:17 +0100

    profile details

--- rewardteacher/src/App.js
+++ rewardteacher/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import ErrorBoundary from './Component/Common/ErrorBoundary';
-import React, { Suspense } from 'react';
+import "./App.css";
+import ErrorBoundary from "./Component/Common/ErrorBoundary";
+import React from "react";
+import StudentProfileTeacher from "./Component/StudentProfileComponent/studentProfile";
 
 function App() {
   return (
     <ErrorBoundary>
-       <Suspense fallback="loading">
+      <StudentProfileTeacher
+        profileDetails="Profile details"
+        babajideLawal="Babajide Lawal"
+        alumniEkoGrammarSchool="Alumni (Eko Grammar school)"
+        otherInfo="Other Info"
+        babajideLawalGmailCom="babajidelawal@gmail.com"
+        phone="08098556634"
+        appreciateStudent="Appreciate Student"
+      />
+      {/* <Suspense fallback="loading">
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
@@ -23,8 +32,8 @@ function App() {
             </a>
           </header>
         </div>
-       </Suspense>  
-    </ErrorBoundary>  
+      </Suspense> */}
+    </ErrorBoundary>
   );
 }
 

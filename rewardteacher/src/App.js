c8d7cd493331bcd1de4fac94c0d249df919e4da3
Iphy <ifunanya.ugwuja@decagon.dev>
2022-08-23 14:57:13 +0100

    resolve

--- rewardteacher/src/App.js
+++ rewardteacher/src/App.js
@@ -1,32 +1,45 @@
-import ErrorBoundary from "./Component/Common/ErrorBoundary";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import ErrorBoundary from "./Component/Common/ErrorBoundary";
 import React, { Suspense } from "react";
-import EditStudentProfile from "./pages/student/EditStudentProfile";
-import { BrowserRouter as Router, Route,Routes, Switch } from "react-router-dom";
 import { HomePage } from "./Component/homePage/HomePage";
 import GlobalFonts from "../src/assets/fonts/fonts";
-
+import EditStudentProfile from "./pages/student/EditStudentProfile";
+// function App() {
+//   return (
+//     <Router>
+//       <ErrorBoundary>
+//         <Suspense fallback="loading">
+//           <Switch>
+//             <Route path="/student-profile">
+//               <EditStudentProfile />
+//             </Route>
+//           </Switch>
+//         </Suspense>
+//       </ErrorBoundary>
+//     </Router>
 function App() {
   return (
     <ErrorBoundary>
       <Suspense fallback="loading">
-
         <div className="App">
-          <header className="App-header">
-          </header>
+          <header className="App-header"></header>
         </div>
       </Suspense>
+
+      {/* <switch>
+        <Route path="/student-profile">
+          <EditStudentProfile />
+        </Route>
+      </switch> */}
 
       <Router>
         <switch>
           <Suspense fallback="loading">
             <GlobalFonts />
-            <Routes>
+            <Route>
               <Route path="/" element={<HomePage />} />
-                <Route path="/student-profile">
-              <EditStudentProfile />
             </Route>
-            </Routes>
           </Suspense>
         </switch>
       </Router>

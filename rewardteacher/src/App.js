b4018e1bfd03fb2feaf381f7816337fe8b157bb8
Erhieyovwe Mary <mary.erhieyovwe4christ@gmail.com>
2022-08-23 18:43:17 +0100

    conflict resolution

--- rewardteacher/src/App.js
+++ rewardteacher/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import StudentProfileTeacher from "./Component/StudentProfileComponent/studentProfile";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ErrorBoundary from "./Component/Common/ErrorBoundary";
 import React, { Suspense } from "react";
@@ -8,6 +9,13 @@ import GlobalFonts from "../src/assets/fonts/fonts";
 function App() {
   return (
     <ErrorBoundary>
+      <Suspense fallback="loading">
+        <div className="App">
+          {/* <header className="App-header">
+          </header> */}
+        </div>
+      </Suspense>
+
       <Router>
         <GlobalFonts />
         <Route path="/" element={<HomePage />} />

20044c6222736858e0a8ec3fb666d335a90386c4
Akinkunmi8 <akinkunmiOlawale8@gmail.com>
2022-08-23 20:27:31 +0100

    Teacher's Notification and Student's Notifications screen feature

--- rewardteacher/src/Component/Notifications/TeacherNotifications.js
+++ rewardteacher/src/Component/Notifications/TeacherNotifications.js
@@ -1,6 +1,6 @@
 import styles from './TeacherNotifications.module.css';
 
-export default function TeacherNotifications() {
+export default function TeacherNotifications() { //bodys
     return (
         <div className={styles['notification-border']}>
             <h1 className={styles.header}>Notifications</h1>

21806bf222b722bef4c0d36edf9db9b6707a6a37
Omolara Olayiwola <ollylarie@gmail.com>
2022-08-23 10:57:14 +0100

    final commit

--- rewardteacher/src/App.js
+++ rewardteacher/src/App.js
@@ -1,7 +1,6 @@
 
 import ErrorBoundary from './Component/Common/ErrorBoundary';
 import React, { Suspense } from "react";
-import Reward from './Component/reward/Reward';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
        <Suspense fallback="loading">
         <div className="App">
          
-        <Reward />
         </div>
        </Suspense>  
     </ErrorBoundary>  

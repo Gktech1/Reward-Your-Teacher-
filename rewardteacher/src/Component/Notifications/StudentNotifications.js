a05f823cca567f216bbdca99d3554bc7db4c2010
Akinkunmi8 <akinkunmiOlawale8@gmail.com>
2022-08-23 06:54:17 +0100

    Teacher's Notification and Student's Notifications screen feature

---
+++ rewardteacher/src/Component/Notifications/StudentNotifications.js
@@ -0,0 +1,29 @@
+import styles from './StudentNotifications.module.css';
+import Emoji from './Emoji.svg';
+
+
+export default function StudentNotifications() {
+    return (
+        <div className={styles['notification-border']}>
+            <h1 className={styles.header}>Notifications</h1>
+            <hr></hr>
+
+            <div className={styles['notification-message2']}>
+               <p><span className={styles['day-message']}>Today, 10:15AM</span>
+                <span className={styles['claimed-message']}>Claimed</span></p>
+                <p className={styles['fund-message']}>You sent money to Babatunde</p>
+            </div>
+            <hr/>
+            <div className={styles['notification-message2']}>
+                <span className={styles['day-message']}>Today, 10:15AM</span>
+                <p className={styles['fund-message']}>You funded your wallet with N12,200</p>
+            </div>
+            <hr/>
+            <div className={styles['notification-message3']}>
+                <span className={styles['day-message']}>Today, 10:15AM</span>                
+                <p className={styles['appriciated-message']}>Cythian Morgan appreciated you <img className={styles.emoji} src={Emoji} /></p>
+            </div>
+        </div>
+    )
+}
+

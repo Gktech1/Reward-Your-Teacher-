7020e10abddafac910f3c7d623c0970e331c55f2
Omolara Olayiwola <ollylarie@gmail.com>
2022-08-24 08:19:28 +0100

    SuccessAlert screen

--- rewardteacher/src/App.js
+++ rewardteacher/src/App.js
@@ -1,15 +1,14 @@
-
-
 import ErrorBoundary from './Component/Common/ErrorBoundary';
 import React, { Suspense } from "react";
+import SuccessAlert from './Component/messagescreen/SuccessAlert';
 import "./App.css";
 function App() {
   return (
     <ErrorBoundary>
       <Suspense fallback="loading">
+        <SuccessAlert />
         <div className="App">
-          <header className="App-header">
-          </header>
+          <header className="App-header"></header>
         </div>
       </Suspense>
     </ErrorBoundary>

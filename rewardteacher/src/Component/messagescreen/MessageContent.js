0d941b1005c456a4b94b25277a2edd5f1a2fa161
Olukunle Edun <favourblessing1@gmail.com>
2022-09-07 13:33:27 +0100

    new branch

--- rewardteacher/src/Component/messagescreen/MessageContent.js
+++ rewardteacher/src/Component/messagescreen/MessageContent.js
@@ -1,116 +1,85 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { SidebarData } from './SideData';
-import { MessageContainer, MessageContainerBodyRight, MessageContainerBody, MessageTextBox, Send} from "./MessageStyle"
+import { MessageContainer, MessageContainerBodyRight, MessageContainerBody, MessageTextBox, Send } from "./MessageStyle"
 import ellipse from '../../assets/Ellipse1.svg';
 import image from '../../assets/Ellipse2.svg';
 import mark from '../../assets/tick.svg';
 import sender from '../../assets/sent.png';
 import './MessageStyle.css';
+import axios from "axios"
+import MessageSideBar from './MessageSideBar';
+
+function MessageContent({ messages, sendMessage }) {
+    const messageRef = useRef();
+    const [message, setMessage] = useState('');
+    const [myProfileImage, setMyProfileImage] = useState('');
+    const [userProfileImage, setUserProfileImage] = useState('');
+
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
 
-function MessageContent() {
+    useEffect(() => {
+        if (messageRef && messageRef.current) {
+            const { scrollHeight, clientHeight } = messageRef.current;
+            messageRef.current.scrollTo({ left: 0, top: scrollHeight - clientHeight, behavior: 'smooth' });
+        }
+    }, [message]);
+
+    // UseEffect for my profile image
+    useEffect(() => {
+        const res = axios.get('image_endpoint_on_coludinary')
+        setMyProfileImage(res)
+    }, [])
+
+    // UseEffect for user profile image
+    useEffect(() => {
+        const res = axios.get('image_endpoint_on_coludinary')
+        setUserProfileImage(res)
+    }, [])
+
+    //console.log('bug',message)
+
     return (
-        <div style={{ border: '1px solid grey', height: 'calc(100% - 1rem)', margin: '1rem 1rem 0 -1rem' }}>
-            <MessageContainer >
-                <h1>Taiwo Fola</h1>
-                <hr />
-                <MessageContainerBody>
-                 <img src={ellipse} alt="wantedimage" />
-                    <div className='body'>
-                        {SidebarData[0].body}
+
+        <div style={{ border: '1px solid grey', height: '100%', paddingTop: '1rem', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+      
+
+            <MessageContainer>
+                {messages.map((m, index) =>
+                    <div className='msg-holder'>
+                        <img src={image} alt="wanted" style={{ paddingRight: '1rem', height: '3rem', width: '3rem', marginTop: '1rem', marginLeft: '1rem' }} />
+                        <div key={index} className='user-message'>
+                            <span className='message bg-primary'>
+                                {m.message}
+                            </span>
+                            <span className='from-user'><span style={{ color: '#0f7f0f' }}>{time} ✓✓</span></span>
+                        </div>
                     </div>
-                </MessageContainerBody>
-
-
-                <div style={{ display: 'flex', height: '5px' }}>
-                    <div style={{ flex: '12rem' }}>
-
-                    </div>
-                    <div>
-                        <p style={{ color: '#C4C4C4', opacity: '0.8' }}> 8:16a.m <img src={mark} alt="tickimage" style={{ marginRight: '1.2rem', }} /></p>
-                    </div>
-                </div>
-
-                <MessageContainerBodyRight>
-                    <div className='body'>
-                        {SidebarData[0].body}
-                    </div>
-                    <img src={image} alt="wantedimage" style={{ marginLeft: '0.5rem' }} />
-
-
-                </MessageContainerBodyRight>
-
-
-                <div style={{ display: 'flex', height: '5px' }}>
-                    <div style={{ flex: '12rem' }}>
-
-                    </div>
-                    <div>
-                        <p style={{ color: '#C4C4C4', opacity: '0.8' }}> 11:36p.m <img src={mark} alt="tickimage" style={{ marginRight: '5rem' }} /></p>
-                    </div>
-                </div>
-
-                <MessageContainerBody>
-                    <img src={ellipse} alt="wantedimage" />
-
-                    <div className='body'>
-                        {SidebarData[0].body}
-                    </div>
-
-                </MessageContainerBody>
-
-
-                <div style={{ display: 'flex', height: '5px' }}>
-                    <div style={{ flex: '12rem' }}>
-
-                    </div>
-                    <div>
-                        <p style={{ color: '#C4C4C4', opacity: '0.8' }}> 8:16a.m <img src={mark} alt="tickimage" style={{ marginRight: '1.2rem', }} /></p>
-                    </div>
-                </div>
-
-                <MessageContainerBodyRight>
-                    <div className='body'>
-                        {SidebarData[0].body}
-                    </div>
-                    <img src={image} alt="wantedimage" style={{ marginLeft: '0.5rem' }} />
-
-
-                </MessageContainerBodyRight>
-
-
-                <div style={{ display: 'flex', height: '5px' }}>
-                    <div style={{ flex: '12rem' }}>
-
-                    </div>
-                    <div>
-                        <p style={{ color: '#C4C4C4', opacity: '0.8' }}> 11:36p.m <img src={mark} alt="tickimage" style={{ marginRight: '5rem' }} /></p>
-                    </div>
-                </div>
-
-
+                )}
             </MessageContainer>
-            
-            <MessageTextBox>
 
-                <div className="form-group has-search" style={{ flex: '9rem' }}>
-                    <span className="fa fa-search form-control-feedback"></span>
-                    <input type="text"
-                     className="form-control" placeholder="Type a new message" 
-                     style={{ height: '54px', 
-                     width: '95%', borderRadius: '10px', border: '1px solid grey', flex: '9rem', marginBottom: '1rem' }} />
-                </div>
 
-                <Send>
-                      <img src={sender} alt="send message" />
-                </Send>
-            
-            </MessageTextBox>
+            <div style={{ justifySelf: 'end' }}>
+                <form
+                    onSubmit={e => {
+                        e.preventDefault();
+                        sendMessage(message);
+                        setMessage('');
+                    }}
+                >
+                    <div style={{ padding: '1rem 1rem 0.3rem 1rem' }}>
+                        <div className="" style={{ display: 'flex', }}>
+                            <input type="text" className="form-control" placeholder="Type a new message" style={{ borderRadius: '7px', border: '1px solid grey' }} onChange={e => setMessage(e.target.value)} value={message} />
+                            <button style={{ border: '1px solid grey', borderRadius: '7px', alignSelf: 'center', marginLeft: '0.51rem', padding: '3px auto' }} disabled={!message}>
+                                <img src={sender} alt="send message" style={{ objectFit: 'contain' }} />
+                            </button>
+                        </div>
+                    </div>
+                </form>
 
-                
-
+            </div>
         </div>
-        
     );
 }
 

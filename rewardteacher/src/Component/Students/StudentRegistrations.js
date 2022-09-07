9f2ed3a345b0d891f31ed4bde1a8f90b15087373
Kimbill <ayooluwaa@decagonhq.com>
2022-09-07 18:04:35 +0100

    fixed teachers' registration and login

--- rewardteacher/src/Component/Students/StudentRegistrations.js
+++ rewardteacher/src/Component/Students/StudentRegistrations.js
@@ -92,7 +92,8 @@ const StudentRegistration = () => {
     console.log(isValid);
     if (isValid.email && isValid.password) {
       console.log("valid");
-      const registerUrl = "https://localhost:7166/User/api/v1/RegisterUser";
+      const registerUrl = "https://localhost:7166/Student/api/v1/CreateStudent";
+      
       axios.post(registerUrl, userData).then(
         (response) => {
           console.log(response.data);
